52c65e2a01f1c86fb9ad265de91be2e49d4e5c22
AnhTu-8011 <kinnguyen801@gmail.com>
2025-11-09 10:59:23 +0700

    removed PasswordResetLinkController

--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,6 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-    // removed: forgot/reset password routes
 });
 
 Route::middleware('auth')->group(function () {
