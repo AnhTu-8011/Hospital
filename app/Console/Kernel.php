b949c996c1379d595779854c0e44d249907ab3d2
AnhTu-8011 <kinnguyen801@gmail.com>
2025-11-29 03:56:28 +0700

    nhắc nhở lịch hẹn thành công

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,11 +13,22 @@ class Kernel extends ConsoleKernel
      * Tạo Mailable cho nội dung email.
      * Tạo Console Command chạy mỗi ngày, quét các lịch hẹn ngày mai và gửi mail.
      * Đăng ký command trong Kernel để Laravel Scheduler gọi tự động.
+     * nếu muốn chạy thự liền thì dùng $schedule->command('app:send-appointment-reminder')->everyMinute(); và chạy lệnh php artisan schedule:run
      */
     protected function schedule(Schedule $schedule): void
     {
         // Gửi nhắc lịch mỗi ngày lúc 07:00
-        $schedule->command('app:send-appointment-reminder')->dailyAt('12:43');
+        //$schedule->command('app:send-appointment-reminder')->dailyAt('03:46');
+        $schedule->command('app:send-appointment-reminder')->everyMinute();
     }
 
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
 }
