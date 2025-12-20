675e9bbabce1ab47fad6d518a4badb0c9b2cb689
AnhTu-8011 <kinnguyen801@gmail.com>
2025-12-20 11:08:50 +0700

    up

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -9,7 +9,15 @@ class Patient extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'user_id', 'name', 'email',  'birthdate', 'gender', 'address', 'phone', 'insurance_number', 'avatar'
+        'user_id', 
+        'name', 
+        'email',  
+        'birthdate', 
+        'gender', 
+        'address', 
+        'phone', 
+        'insurance_number', 
+        'avatar'
     ];
 
     protected $casts = [
@@ -23,22 +31,22 @@ class Patient extends Model
  /**
      * Tự động điền name và email từ user khi tạo patient mới
      */
-    protected static function boot()
-    {
-        parent::boot();
-        static::creating(function ($patient) {
-            if ($patient->user) {
-                // Nếu có user và name chưa được điền, lấy từ user
-                if (empty($patient->name)) {
-                    $patient->name = $patient->user->name;
-                }
-                // Nếu có user và email chưa được điền, lấy từ user
-                if (empty($patient->email)) {
-                    $patient->email = $patient->user->email;
-                }
-            }
-        });
-    }
+    // protected static function boot()
+    // {
+    //     parent::boot();
+    //     static::creating(function ($patient) {
+    //         if ($patient->user) {
+    //             // Nếu có user và name chưa được điền, lấy từ user
+    //             if (empty($patient->name)) {
+    //                 $patient->name = $patient->user->name;
+    //             }
+    //             // Nếu có user và email chưa được điền, lấy từ user
+    //             if (empty($patient->email)) {
+    //                 $patient->email = $patient->user->email;
+    //             }
+    //         }
+    //     });
+    // }
 
     public function appointments()
     {
